b9edb2f0ed530826f2f8d90a18a0c3d93a5061fc
JamesYang <43404899+Yangliujie@users.noreply.github.com>
2021-05-25 20:40:19 +0800

    profile
    完成了profile和登录页面的wxml和wxss设计，接下来就是接口设计和交互

---
+++ pages/login_register/loginAndRegister.vue
@@ -0,0 +1,103 @@
+<template>
+	<view>
+		<view class="content">
+			<!-- 登录注册页面图标 -->
+			<view class="avatorWrapper">
+				<view class="avator">
+					<image class="img" src="../../static/游泳池.png" mode="widthFix"></image>
+				</view>
+			</view>
+			
+			<!-- 登录注册信息表单 -->
+			<view class="form">
+				<view class="inputWrapper">
+					<input class="input" type="text" value="" placeholder="请输入手机号"/>
+				</view>
+				<view class="inputWrapper">
+					<input class="input" type="password" value="" placeholder="请输入密码"/>
+				</view>
+				<view class="loginBtn">
+					<text class="btnValue">登录</text>
+				</view>
+				<view class="forgotBtn">
+				    <text>找回密码</text>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+    .content {
+        background: #7dc5eb;
+        width: 100vw;
+        height: 100vh;
+    }
+    .avatorWrapper{
+        height: 30vh;
+        width: 100vw;
+        display: flex;
+        justify-content: center;
+        align-items: flex-end;
+    }
+    .avator{
+        width: 200upx;
+        height: 200upx;
+        overflow: hidden;
+    }
+    .avator .img{
+        width: 100%
+    }
+    .form{
+        padding: 0 100upx;
+        margin-top: 80px;
+    }
+    .inputWrapper{
+        width: 100%;
+        height: 80upx;
+        background: white;
+        border-radius: 20px;
+        box-sizing: border-box;
+        padding: 0 20px;
+        margin-top: 25px;
+    }
+    .inputWrapper .input{
+        width: 100%;
+        height: 100%;
+        text-align: center;
+        font-size: 15px;
+    }
+    .loginBtn{
+        width: 100%;
+        height: 80upx;
+        background: #77B307;
+        border-radius: 50upx;
+        margin-top: 50px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        
+    }
+    .loginBtn .btnValue{
+        color: white;
+    }
+    .forgotBtn{
+        text-align: center;
+        color: #EAF6F9;
+        font-size: 15px;
+        margin-top: 20px;
+    }
+</style>
---
+++ pages/profile/profile.vue
@@ -0,0 +1,88 @@
+<template>
+	
+	<view class="profile">
+		<!-- 顶层图片及头像栏 -->
+		<view class="topBackground">
+			<image src="../../static/logo.png" mode="aspectFit"></image>
+			<view class="flex-item flex-item-V">
+				{{message0}}
+			</view>
+			<view class="flex-item flex-item-V ">
+				<text>关注:{{message1}}&nbsp;&nbsp;&nbsp;粉丝：{{message2}}</text>
+			</view>
+		</view>
+		
+		<!-- 表单1：昵称、等级、关注用户和粉丝数量 -->
+		<view class="firstForm">
+			<uni-list>
+				<button class= type="primary" open-type="getUserInfo"></button>
+				<uni-list-item title="昵称" clickable="true" @click="login" rightText="小白"></uni-list-item>
+				<uni-list-item title="等级" rightText="1"></uni-list-item>
+			</uni-list>
+		</view>
+		
+		
+		<!-- 表单2：设置、收藏夹、隐私、关于我们 -->
+		<view class="secondForm">
+			<uni-list>
+				<uni-list-item title="设置" link=""></uni-list-item>
+				<uni-list-item title="收藏夹" link=""></uni-list-item>
+				<uni-list-item title="隐私" link=""></uni-list-item>
+				<uni-list-item title="通知提醒":show-switch="true" @switchChange="switchChange" switchChecked="true"></uni-list-item>
+				<uni-list-item title="关于我们" link=""></uni-list-item>
+			</uni-list>
+		</view>
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				message0:"userID",
+				message1:30,
+				message2:40
+			}
+		},
+		login:function(){
+			wx.login({
+				success:function(res){
+					if(res.code){
+						wx.request({
+							url:app.serverurl+''
+						})
+					}
+				}
+			})
+		},
+		onLoad:function(){
+			
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+.topBackground{
+	width: 750rpx;
+	height: 450rpx;
+	background-image:url(../../static/background.jpeg);
+	background-size: 100% 100%;
+}
+.topBackground>image{
+	width: 100rpx;
+	height: 100rpx;
+	margin: 50rpx 325rpx 140rpx 325rpx;
+}
+.topBackground>view{
+	margin: 20rpx 0;
+	color: #aaaaff;
+	text-align: center;
+}
+.secondForm{
+	margin-top: 3rpx;
+}
+</style>
